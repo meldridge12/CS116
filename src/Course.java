d75afd503b74b6171f17324648174840d3c28ec7
Michaelangelo Eldridge <eldridgem526@gmail.com>
2024-10-30 15:16:09 -0500

    Added constructors to Course class

--- src/Course.java
+++ src/Course.java
@@ -1,5 +1,31 @@
-
-
 public class Course {
+
+    // attributes
+    private String iD;
+    private String name;
+    private int currentNumSeats;
+    private int totalNumSeats;
+    private String[] prerequisites;
+
+    // constants
+
+    // constructors
+    public Course() {
+        iD = "This course has no ID.";
+        name = "This course has no name.";
+        currentNumSeats = 0;
+        totalNumSeats = 0;
+        prerequisites = new String[0];
+    }
+
+    public Course(String tempID, String tempName, int tempCurrent, int tempTotal, String[] tempReqs) {
+        iD = tempID;
+        name = tempName;
+        currentNumSeats = tempCurrent;
+        totalNumSeats = tempTotal;
+        prerequisites = tempReqs;
+    }
+
+    // accessors
 
 }
