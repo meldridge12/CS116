73b57c7a99b94964e35aeea54f34fda41de5eac4
Tri Phan <tripkhan005@gmail.com>
2024-10-31 10:33:14 -0500

    testing a readfile();

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,8 @@
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 public class Student {
     
@@ -8,66 +12,84 @@ public class Student {
     private ArrayList<String> pastCourses;
     private ArrayList<String> futureCourses;
     private int numCourses; 
+    private String filePath;
     
 
 
     // Constructor
     Student() {
-        ID = "No Student ID";
-        name = "No Student name";
-        pastCourses = new ArrayList<>();
-        futureCourses = new ArrayList<>();
-        numCourses = 5;
+        this.ID = "No Student ID";
+        this.name = "No Student name";
+        this.pastCourses = new ArrayList<>();
+        this.futureCourses = new ArrayList<>();
+        this.numCourses = 5;
+        this.filePath = "";
     }
 
-    Student(String ID, String name, ArrayList<String> pastCourses, ArrayList<String> futureCourses, int numCourses) {
+    Student(String ID, String name, ArrayList<String> pastCourses, ArrayList<String> futureCourses, int numCourses, String filePath) {
         this.ID = ID;
         this.name = name;
         this. pastCourses = pastCourses;
         this.futureCourses = futureCourses;
         this.numCourses = numCourses;
+        this.filePath = filePath;
     }
 
     
 
     // Accessors
     public String getID() {
-        return ID;
+        return this.ID;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public ArrayList<String> getPastCourses() {
-        return pastCourses;
+        return this.pastCourses;
     }
 
     public ArrayList<String> getFutureCourses() {
-        return futureCourses;
+        return this.futureCourses;
     }
 
     // Mutators
     public void setID(String newID) {
-        ID = newID;
+        this.ID = newID;
     }
     
     public void setName(String newName) {
-        name = newName;
+        this.name = newName;
     }
 
 
     // Methods
+    public void readFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+         } catch (IOException e) {
+                System.out.println("An error occurred while reading the file: " + e.getMessage());
+                e.printStackTrace();
+            }
+
+    }
+
     public void printPastCourses() {
     
         // Goes through all the past courses and prints them out 
-        for(String element : pastCourses) {
+        for(String element : this.pastCourses) {
             System.out.println(element);
         }
     } 
 
     public boolean checkRegistrationForCourses(String course) {
         
+
+
         
         // Still needs to check if the student has completed the prerequisit courses
         
@@ -78,13 +100,13 @@ public class Student {
 
         
         // Checks to see if the student has enough room in their schedule 
-        if(numCourses <= 5) {
+        if(this.numCourses <= 5) {
             System.out.println("Student has registered the max amount of courses");
             return false;
         }
         
         // Goes through past courses to see if student has already taken the course
-        for(String element : pastCourses) {
+        for(String element : this.pastCourses) {
             if(element.equals(course)) {
                 System.out.println("The student has already taken this course");
                 return false;
@@ -92,7 +114,7 @@ public class Student {
         }
 
         // Goes through future courses to check if student has registered for this couse already
-        for(String element : futureCourses) {
+        for(String element : this.futureCourses) {
             if(element.equals(course)) {
                 System.out.println("The student has already registered for this course");
                 return false;
@@ -100,9 +122,9 @@ public class Student {
         }
 
 
-        futureCourses.add(course);
-        numCourses --;
-        System.out.println("The course registration was sucessful, the student has " + numCourses + " open courses");
+        this.futureCourses.add(course);
+        this.numCourses --;
+        System.out.println("The course registration was sucessful, the student has " + this.numCourses + " open courses");
         
         return true;
     }
@@ -111,7 +133,7 @@ public class Student {
         
         // Goes through future courses to check if student has that course regiestered in order
         // to be able to cancel it, still needs to check if student has registered in previous 
-        for(String element : futureCourses) {
+        for(String element : this.futureCourses) {
             if(element.equals(course)) {
                 System.out.println("The student is able to cancel this course");
                 return true;
