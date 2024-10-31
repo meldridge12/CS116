3ad06ed8ac42d14d0463b6484da3f87ebcca60ed
Tri Phan <tripkhan005@gmail.com>
2024-10-30 21:24:53 -0500

    added more comments and methods

--- src/Student.java
+++ src/Student.java
@@ -51,20 +51,33 @@ public class Student {
 
     // Methods
     public void printPastCourses() {
+    
+        // Goes through all the past courses and prints them out 
         for(int i = 0; i < pastCourses.size(); i ++) {
             System.out.println(pastCourses.indexOf(i));
         }
     } 
 
     public boolean checkRegistrationForCourses(String course) {
+        
+        // Checks to see if the student has enough room in their schedule 
         if(numCourses >= 5) {
             System.out.println("Student has registered the max amount of courses");
             return false;
         }
         
+        // Goes through past courses to see if student has already taken the course
         for(String element : pastCourses) {
             if(element.equals(course)) {
                 System.out.println("The student has already taken this course");
+                return false;
+            }
+        }
+
+        // Goes through future courses to check if student has registered for this couse already
+        for(String element : futureCourses) {
+            if(element.equals(course)) {
+                System.out.println("The student has already registered for this course");
                 return false;
             }
         }
@@ -75,6 +88,20 @@ public class Student {
         return true;
     }
 
+    public boolean cancelCourse(String course) {
+        
+        // Goes through future courses to check if student has that course regiestered in order
+        // to be able to cancel it, still needs to check if student has registered in previous 
+        for(String element : futureCourses) {
+            if(element.equals(course)) {
+                System.out.println("The student is able to cancel this course");
+                return true;
+            }
+        }  
+
+        System.out.println("Student is not able to cancel course");
+        return false;
+    }
 
 
 }
