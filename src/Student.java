3f905d67da8d3834abc8dfdd337a50cf607fc5cb
Tri Phan <tripkhan005@gmail.com>
2024-10-30 20:41:14 -0500

    added more getters and setters

--- src/Student.java
+++ src/Student.java
@@ -4,6 +4,8 @@ public class Student {
     // Attributes
     private String ID;
     private String name; 
+    private String[] pastCourses;
+    private String[] futureCourses;
 
     
 
@@ -12,6 +14,38 @@ public class Student {
     Student() {
         ID = "No Student ID";
         name = "No Student name";
+        pastCourses = new String[0];
+        futureCourses = new String[0];
     }
+
+    
+
+    // Accessors
+    public String getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getPastCourses() {
+        return pastCourses;
+    }
+
+    public String[] getFutureCourses() {
+        return futureCourses;
+    }
+
+    // Mutators
+    public void setID(String newID) {
+        ID = newID;
+    }
+    
+    public void setName(String newName) {
+        name = newName;
+    }
+
+
 
 }
