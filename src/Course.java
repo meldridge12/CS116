74968a1d0b5872927c44672a722fc413185f724d
Michaelangelo Eldridge <eldridgem526@gmail.com>
2024-10-31 11:17:27 -0500

    Added mutators and toString method to Course

--- src/Course.java
+++ src/Course.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Course {
 
     // attributes
@@ -5,20 +7,20 @@ public class Course {
     private String name;
     private int currentNumSeats;
     private int totalNumSeats;
-    private String[] prerequisites;
+    private ArrayList<String> prerequisites;
 
     // constants
 
     // constructors
     public Course() {
-        iD = "This course has no ID.";
-        name = "This course has no name.";
+        iD = "N/A";
+        name = "N/A";
         currentNumSeats = 0;
         totalNumSeats = 0;
-        prerequisites = new String[0];
+        prerequisites = new ArrayList<String>();
     }
 
-    public Course(String tempID, String tempName, int tempCurrent, int tempTotal, String[] tempReqs) {
+    public Course(String tempID, String tempName, int tempCurrent, int tempTotal, ArrayList<String> tempReqs) {
         iD = tempID;
         name = tempName;
         currentNumSeats = tempCurrent;
@@ -27,5 +29,55 @@ public class Course {
     }
 
     // accessors
+    public String getID() {
+        return iD;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getCurrentSeats() {
+        return currentNumSeats;
+    }
+
+    public int getTotalSeats() {
+        return totalNumSeats;
+    }
+
+    public ArrayList<String> getPrereqs() {
+        return prerequisites;
+    }
+
+    // mutators
+    public void setID(String newID) {
+        iD = newID;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public void setCurrentSeats(int newCurrent) {
+        currentNumSeats = newCurrent;
+    }
+
+    public void setTotalSeats(int newTotal) {
+        totalNumSeats = newTotal;
+    }
+
+    public void setNewPrereqs(ArrayList<String> newPreeqs) {
+        prerequisites = newPreeqs;
+    }
+
+    // additional methods
+
+    // 2String
+    public String toString() {
+        String course = iD + "/t" + name + "/t" + currentNumSeats + "/" + totalNumSeats + "/t";
+        for (String singlePrereq : prerequisites) {
+            course += singlePrereq + ",";
+        }
+        return course;
+    }
 }
